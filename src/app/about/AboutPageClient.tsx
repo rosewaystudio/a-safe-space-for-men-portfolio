75572fc6418a66d933a5d4e017a5c029adfd454a
Andrea Frazier <contact@rosewaystudio.com>
2026-04-22 20:50:17 -0400

    change about page photo container to remove white space

--- src/app/about/AboutPageClient.tsx
+++ src/app/about/AboutPageClient.tsx
@@ -94,7 +94,7 @@ export default function AboutPageClient() {
                   src="/images/BMIL_quote.JPG" 
                   alt="William's quote about the importance of mental health support for men"
                   fill
-                  className="object-contain"
+                  className="object-cover"
                   sizes="(max-width: 768px) 100vw, 50vw"
                 />
               </div>
